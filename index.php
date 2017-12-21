ea456237e0fd295606c19eed86bff968d72b0a10
Ruben Smit <rubensmit@live.nl>
2017-12-21 16:13:15 +0100

    Added HTML

--- index.php
+++ index.php
@@ -1,5 +1,73 @@
 <?php
-include 'header.php';
+echo "
+<!DOCTYPE html>
+<html lang="en">
 
-include 'footer.php';
+	<head>
+	<meta charset="utf-8">
+
+	<title>Gebakken Lucht</title>
+	<meta name="Welcome to a webshop that sells air!" content="Gebakken Lucht">
+	<meta name="Gebakken Lucht, Luchtkasteel, Special Air" content="Gebakken Lucht webshop">
+
+	<link rel="stylesheet" type="text/css" href="styles/stylesheet.css"/>
+
+	</head>
+
+	<body>
+
+	<div class ="main">
+
+
+		<div class="header">
+
+		 <!-- Header image moet zo breed zijn als de main (600px breed). -->
+
+			<img class="header-image" src="images/voor_achter.jpg">
+
+		</div>
+
+		<div class="titlebar">
+
+			<a href="index.html">
+				Home
+			</a>
+
+			<a href="webshop.html">
+				Webshop
+			</a>
+
+			<a href="contact.html">
+				Contact
+			</a>
+
+
+		</div>
+
+		<div class="content">
+
+			<h1>Welkom bij Gebakken Lucht BV!</h1>
+
+			<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean interdum sapien quam, at iaculis sem semper ac. Nam vitae mi nisl. Sed iaculis tincidunt purus, in laoreet velit. Praesent nec eros at tortor mattis facilisis a eu lectus. Integer sem justo, fringilla et leo vitae, rhoncus ullamcorper risus. Sed non rutrum nunc. Donec mi quam, tristique id eleifend at, tincidunt et massa. Nulla egestas elementum ante.</p>
+
+			<p>Vivamus accumsan tellus eu sem hendrerit elementum. Phasellus quis scelerisque quam. Mauris mauris sem, lacinia vel arcu in, malesuada pretium eros. Pellentesque ut porta ex. Nam dapibus nunc quis nunc tincidunt, in egestas justo bibendum. Proin elementum sapien a tortor semper, ac aliquam est commodo. Curabitur lacinia ac libero in volutpat. Etiam tristique nulla nisl, in egestas massa convallis volutpat. Morbi ut ante lobortis, blandit nulla a, auctor ipsum. Donec euismod orci felis. Morbi vel rutrum dolor, ut vehicula enim. Nunc at odio ut diam pretium tristique at a purus. Maecenas dignissim nibh dui, in placerat nisi blandit a.</p>
+
+			<p>
+			Integer commodo sit amet enim quis lobortis. Integer malesuada ligula et metus pretium consectetur. Etiam ac nulla dapibus, consectetur lacus at, vestibulum velit. Fusce id magna sodales, placerat sem a, blandit diam. Nulla euismod purus fringilla, imperdiet mi at, placerat augue. Duis pretium pulvinar ante eget pellentesque. Nulla pharetra egestas nulla, non egestas lacus bibendum id. Sed purus nibh, tristique et quam vehicula, imperdiet faucibus purus. Quisque egestas orci lectus, a pellentesque risus aliquet a. Sed convallis leo a orci pellentesque, ut mattis metus mollis. Maecenas faucibus eu velit quis lacinia.
+			</p>
+
+		</div>
+
+		<div class="footer">
+			All rights reserved &copy; Gebakken Luchtt&reg; BV. </br> Gebakken Lucht&reg; is a registered trademark by Gebakken Lucht BV.
+		</div>
+
+
+
+</div>
+
+
+	</body>
+</html>
+";
 ?>
