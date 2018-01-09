9148b681c3ae291c2ec4df72871d7eccce439c16
Ruben Smit <rubensmit@live.nl>
2018-01-09 10:33:13 +0100

    Updated file layout

--- footer.php
+++ footer.php
@@ -1,1 +1,11 @@
-<?php  ?>
+<?php
+echo '
+      </div>
+      <div class="footer">
+        All rights reserved &copy; Gebakken Luchtt&reg; BV. </br> Gebakken Lucht&reg; is a registered trademark by Gebakken Lucht BV.
+      </div>
+    </div>
+  </body>
+</html>
+';
+?>
--- header.php
+++ header.php
@@ -1,1 +1,49 @@
-<?php  ?>
+<?php
+echo '
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+	<meta charset="utf-8">
+
+	<title>Gebakken Lucht</title>
+	<meta name="Welcome to a webshop that sells air!" content="Gebakken Lucht">
+	<meta name="Gebakken Lucht, Luchtkasteel, Special Air" content="Gebakken Lucht webshop">
+
+	<link rel="stylesheet" type="text/css" href="styles/stylesheet.css"/>
+
+	</head>
+
+	<body>
+
+	<div class ="main">
+
+
+		<div class="header">
+
+		 <!-- Header image moet zo breed zijn als de main (600px breed). -->
+
+			<img class="header-image" src="images/voor_achter.jpg">
+
+		</div>
+
+		<div class="titlebar">
+
+			<a href="index.php">
+				Home
+			</a>
+
+			<a href="/products">
+				Producten
+			</a>
+
+			<a href="contact.php">
+				Contact
+			</a>
+
+
+		</div>
+
+		<div class="content">
+';
+?>
