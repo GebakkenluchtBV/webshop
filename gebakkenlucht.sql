5c971e42766b4e8bdd6c56ef4eec694a5465d43b
Ruben Smit <rubensmit@live.nl>
2018-01-11 13:14:56 +0100

    Changed database

--- gebakkenlucht.sql
+++ gebakkenlucht.sql
@@ -1,34 +1,35 @@
 /* Drop Tables */
+SET FOREIGN_KEY_CHECKS=0;
 
-DROP TABLE IF EXISTS `categories` CASCADE
+DROP TABLE IF EXISTS `products` CASCADE
 ;
 
 DROP TABLE IF EXISTS `customers` CASCADE
 ;
 
-DROP TABLE IF EXISTS `order_products` CASCADE
-;
-
 DROP TABLE IF EXISTS `orders` CASCADE
 ;
 
-DROP TABLE IF EXISTS `products` CASCADE
+DROP TABLE IF EXISTS `categories` CASCADE
+;
+
+DROP TABLE IF EXISTS `order_products` CASCADE
 ;
 
 /* Create Tables */
 
 CREATE TABLE `categories`
 (
-	`id` INT AUTO_INCREMENT,
-	`name` VARCHAR(50),
-	PRIMARY KEY (`id` ASC)
+	`category_id` INT AUTO_INCREMENT,
+	`category` VARCHAR(50),
+	PRIMARY KEY (`category_id` ASC)
 )
 
 ;
 
 CREATE TABLE `customers`
 (
-	`id` INT AUTO_INCREMENT,
+	`customer_id` INT AUTO_INCREMENT,
 	`firstName` VARCHAR(50),
 	`lastName` VARCHAR(50),
 	`address` VARCHAR(50),
@@ -38,63 +39,67 @@ CREATE TABLE `customers`
 	`phoneNumber` VARCHAR(50) NULL,
 	`IBAN` VARCHAR(37) NULL,
 	`IBANholder` VARCHAR(50) NULL,
-	PRIMARY KEY (`id` ASC)
-)
-
-;
-
-CREATE TABLE `order_products`
-(
-	`id` INT AUTO_INCREMENT,
-	`amount` INT DEFAULT 1,
-	`order_id` INT,
-	`product_id` INT,
-	PRIMARY KEY (`id` ASC),
-	FOREIGN KEY (`order_id`) REFERENCES orders(`id`),
-	FOREIGN KEY (`product_id`) REFERENCES products(`id`)
+	`username` VARCHAR(50) UNIQUE,
+	`password` VARCHAR(50),
+	`isAdmin` BOOLEAN,
+	PRIMARY KEY (`customer_id` ASC)
 )
 
 ;
 
 CREATE TABLE `orders`
 (
-	`id` INT AUTO_INCREMENT,
+	`order_id` INT AUTO_INCREMENT,
 	`orderedAt` TIMESTAMP,
 	`status` INT DEFAULT 1,
 	`totalPrice` INT,
 	`customer_id` INT,
-	PRIMARY KEY (`id` ASC),
-	FOREIGN KEY (`customer_id`) REFERENCES customers(`id`)
+	PRIMARY KEY (`order_id` ASC),
+	FOREIGN KEY (`customer_id`) REFERENCES customers(`customer_id`)
 )
 
 ;
 
 CREATE TABLE `products`
 (
-	`id` INT AUTO_INCREMENT,
+	`product_id` INT AUTO_INCREMENT,
 	`name` VARCHAR(50),
 	`price` INT,
 	`description` LONGTEXT NULL,
 	`amountInStock` INT NULL,
 	`category_id` INT,
-	PRIMARY KEY (`id` ASC),
-	FOREIGN KEY (`category_id`) REFERENCES categories(`id`)
+	PRIMARY KEY (`product_id` ASC),
+	FOREIGN KEY (`category_id`) REFERENCES categories(`category_id`)
 )
 
 ;
 
+CREATE TABLE `order_products`
+(
+	`order_products_id` INT AUTO_INCREMENT,
+	`amount` INT DEFAULT 1,
+	`order_id` INT,
+	`product_id` INT,
+	PRIMARY KEY (`order_products_id` ASC),
+	FOREIGN KEY (`order_id`) REFERENCES orders(`order_id`),
+	FOREIGN KEY (`product_id`) REFERENCES products(`product_id`)
+)
+
+;
+
+SET FOREIGN_KEY_CHECKS=1;
 
 /* Seed tables */
 
 
 INSERT INTO `categories` (
-	`name`
+	`category`
 ) VALUES (
 	"luchtflessen"
 );
 
 INSERT INTO `categories` (
-	`name`
+	`category`
 ) VALUES (
 	"luchtkastelen"
 );
@@ -103,4 +108,130 @@ INSERT INTO `products` (
 	`name`, `price`, `description`, `amountInStock`, `category_id`
 ) VALUES (
 	"Gebakken Lucht", 1500, "Zeer praktisch, gebakken lucht vers uit een fles!", 50, 1
+);
+
+INSERT INTO `products` (
+	`name`, `price`, `description`, `amountInStock`, `category_id`
+) VALUES (
+	"XXL luchtkasteel", 15000, "Super lomp groot kasteel voor in de lucht.", 2, 2
+);
+
+INSERT INTO `customers` (
+`firstName` ,
+`lastName` ,
+`address` ,
+`postalCode` ,
+`city` ,
+`country` ,
+`phoneNumber` ,
+`IBAN` ,
+`IBANholder`,
+`username`,
+`password`,
+`isAdmin`
 )
+VALUES (
+'Theo', 'de Vries', 'Straatweg 30', '1234AB', 'Enschede', 'Nederland', '06-12345678', 'NL12ABCD3456789012', 't. de vries', 'theo', 'password', 0
+);
+
+INSERT INTO `customers` (
+`firstName` ,
+`lastName` ,
+`address` ,
+`postalCode` ,
+`city` ,
+`country` ,
+`phoneNumber` ,
+`IBAN` ,
+`IBANholder`,
+`username`,
+`password`,
+`isAdmin`
+)
+VALUES (
+'Hans', 'Janssen', 'Hoofdstraat 22', '5432EZ', 'Rotterdam', 'Nederland', '06-98765432', 'NL09QWER7654321098', 'h. janssen', 'hans', 'password', 0
+);
+
+INSERT INTO `customers` (
+`firstName` ,
+`lastName` ,
+`address` ,
+`postalCode` ,
+`city` ,
+`country` ,
+`phoneNumber` ,
+`IBAN` ,
+`IBANholder`,
+`username`,
+`password`,
+`isAdmin`
+)
+VALUES (
+'Admin', 'Administrator', 'Saxion', '5432EZ', 'Enschede', 'Nederland', '06-98765432', 'NL09QWER7654321098', 'admin', 'admin', 'password', 1
+);
+
+INSERT INTO `orders` (
+`orderedAt` ,
+`status` ,
+`totalPrice` ,
+`customer_id`
+)
+VALUES (
+CURRENT_TIMESTAMP , '1', '1500', '1'
+);
+
+INSERT INTO `order_products` (
+`amount` ,
+`order_id` ,
+`product_id`
+)
+VALUES (
+'1', '1', '1'
+);
+
+INSERT INTO `orders` (
+`orderedAt` ,
+`status` ,
+`totalPrice` ,
+`customer_id`
+)
+VALUES (
+CURRENT_TIMESTAMP , '1', '15000', '1'
+);
+
+INSERT INTO `order_products` (
+`amount` ,
+`order_id` ,
+`product_id`
+)
+VALUES (
+'1', '2', '2'
+);
+
+INSERT INTO `orders` (
+`orderedAt` ,
+`status` ,
+`totalPrice` ,
+`customer_id`
+)
+VALUES (
+CURRENT_TIMESTAMP , '1', '16500', '2'
+);
+
+INSERT INTO `order_products` (
+`amount` ,
+`order_id` ,
+`product_id`
+)
+VALUES (
+'1', '3', '1'
+);
+
+INSERT INTO `order_products` (
+`amount` ,
+`order_id` ,
+`product_id`
+)
+VALUES (
+'1', '3', '2'
+);
